af2d2db28bfc355ecd2a2e2841b22f84db519e29
Matheus Muccio <matheus_muccio@outlook.com>
2022-03-07 17:58:09 +0000

    Atualizando

--- Tableless.java
+++ Tableless.java
@@ -1,18 +1,21 @@
-
+import java.util.Scanner;
 
 public class Tableless {
 
     public static void main(String[] args){
 
-        int valor1 = 1;
-        int valor2 = 2;
+        int n1;
+        int n2;
+        
+        Scanner leitura = new Scanner(System.in);
 
-        System.out.println((valor1 > valor2 ? "1 é maior" : "2 é maior"));
+        System.out.println("Digite o primeiro número: ");
+        n1 = leitura.nextInt();
 
-        if (valor1 > valor2) {
-            System.out.println("1 é maior");
-        } else {
-            System.out.println("2 é maior");
-        }
+        System.out.println("Digite o segundo número: ");
+        n2 = leitura.nextInt();
+
+        System.out.println((n1 > n1 ? n1 + " é o maior número" : n2 + " é o maior número"));
+
     }
 }
