01b16d1ff004e8a2971209ace82513fce752cd87
Matheus Muccio <matheus_muccio@outlook.com>
2022-03-07 17:36:39 +0000

    Salvando

---
+++ Tableless.java
@@ -0,0 +1,18 @@
+
+
+public class Tableless {
+
+    public static void main(String[] args){
+
+        int valor1 = 1;
+        int valor2 = 2;
+
+        System.out.println((valor1 > valor2 ? "1 é maior" : "2 é maior"));
+
+        if (valor1 > valor2) {
+            System.out.println("1 é maior");
+        } else {
+            System.out.println("2 é maior");
+        }
+    }
+}
